0c9d258e137e6e45b0e0d4cba9f9f77a6e757fe0
sciencegum5 <32943597+sciencegum5@users.noreply.github.com>
2019-01-23 13:59:25 -1000

    Update restaurantScript.js

--- restaurantScript.js
+++ restaurantScript.js
@@ -1,11 +1,11 @@
-var grabElement = "document.getElementById"
+var $ = "document"
 var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var menudata = JSON.parse(this.responseText);
         console.log("json values updated");
         console.log(menudata.bento[2].ingredients[0]);
-        grabElement('itemOneName').innerHtml = menudata.bento
+        $.getElementById('itemOneName').innerHtml = menudata.bento;
       }
     };
     xmlhttp.open("GET","Items.json",true);
