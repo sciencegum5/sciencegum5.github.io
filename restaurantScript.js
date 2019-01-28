6e963a64c033e95a279e4013d6aa003eb2f28231
sciencegum5 <32943597+sciencegum5@users.noreply.github.com>
2019-01-28 13:51:10 -1000

    Update restaurantScript.js

--- restaurantScript.js
+++ restaurantScript.js
@@ -5,28 +5,28 @@ xmlhttp.onreadystatechange = function () {
     var menudata = JSON.parse(this.responseText);
     console.log("Updating JSON Values");
     $.getElementById('itemOneName').innerHTML = menudata.menuItems[0].salmonBurger[0].name
-    $.getElementById('itemOneDesc').innerHTML = menudata.menuItems[0].salmonBurger[0].price
-    $.getElementById('itemOnePrice').innerHTML = menudata.menuItems[0].salmonBurger[0].description
+    $.getElementById('itemOneDesc').innerHTML = menudata.menuItems[0].salmonBurger[0].description
+    $.getElementById('itemOnePrice').innerHTML = "$" + menudata.menuItems[0].salmonBurger[0].price
     //end one
     $.getElementById('itemTwoName').innerHTML = menudata.menuItems[0].balmonBurger[0].name
-    $.getElementById('itemTwoDesc').innerHTML = menudata.menuItems[0].balmonBurger[0].price
-    $.getElementById('itemTwoPrice').innerHTML = menudata.menuItems[0].balmonBurger[0].description
+    $.getElementById('itemTwoDesc').innerHTML = menudata.menuItems[0].balmonBurger[0].description
+    $.getElementById('itemTwoPrice').innerHTML = "$" + menudata.menuItems[0].balmonBurger[0].price
     //end two
     $.getElementById('itemThreeName').innerHTML = menudata.menuItems[0].galmonBurger[0].name
-    $.getElementById('itemThreeDesc').innerHTML = menudata.menuItems[0].galmonBurger[0].price
-    $.getElementById('itemThreePrice').innerHTML = menudata.menuItems[0].galmonBurger[0].description
+    $.getElementById('itemThreeDesc').innerHTML = menudata.menuItems[0].galmonBurger[0].description
+    $.getElementById('itemThreePrice').innerHTML = "$" + menudata.menuItems[0].galmonBurger[0].price
     //end three
     $.getElementById('itemFourName').innerHTML = menudata.menuItems[0].palmonBurger[0].name
-    $.getElementById('itemFourDesc').innerHTML = menudata.menuItems[0].palmonBurger[0].price
-    $.getElementById('itemFourPrice').innerHTML = menudata.menuItems[0].palmonBurger[0].description
+    $.getElementById('itemFourDesc').innerHTML = menudata.menuItems[0].palmonBurger[0].description
+    $.getElementById('itemFourPrice').innerHTML = "$" + menudata.menuItems[0].palmonBurger[0].price
     //end four
     $.getElementById('itemFiveName').innerHTML = menudata.menuItems[0].zalmonBurger[0].name
-    $.getElementById('itemFiveDesc').innerHTML = menudata.menuItems[0].zalmonBurger[0].price
-    $.getElementById('itemFivePrice').innerHTML = menudata.menuItems[0].zalmonBurger[0].description
+    $.getElementById('itemFiveDesc').innerHTML = menudata.menuItems[0].zalmonBurger[0].description
+    $.getElementById('itemFivePrice').innerHTML = "$" + menudata.menuItems[0].zalmonBurger[0].price
     //end five
     $.getElementById('itemSixName').innerHTML = menudata.menuItems[0].calmonBurger[0].name
-    $.getElementById('itemSixDesc').innerHTML = menudata.menuItems[0].calmonBurger[0].price
-    $.getElementById('itemSixPrice').innerHTML = menudata.menuItems[0].calmonBurger[0].description
+    $.getElementById('itemSixDesc').innerHTML = menudata.menuItems[0].calmonBurger[0].description
+    $.getElementById('itemSixPrice').innerHTML = "$" + menudata.menuItems[0].calmonBurger[0].price
     //end six
     console.log("JSON values updated");
   }
