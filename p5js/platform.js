78eb741f5602863c0be673484a6545b345b6a319
Your Name <you@example.com>
2019-04-18 13:12:12 -1000

    yes

--- p5js/platform.js
+++ p5js/platform.js
@@ -1,11 +1,11 @@
 function platform() {
     this.height = 100;
     this.width = 100;
-    this.x = Math.floor((Math.random() * w/1.7) );
-    this.y = Math.floor((Math.random() * h/1.7 ));
+    this.x = Math.floor((Math.random() * w/1.7) + w/10);
+    this.y = Math.floor((Math.random() * h/1.7 ) +h/10);
     this.hit = 0;
     this.show = function () {
-         imageMode(CENTER);
+         imageMode(CORNER);
         image(sat, this.x, this.y, this.width, this.height);
 
     }
